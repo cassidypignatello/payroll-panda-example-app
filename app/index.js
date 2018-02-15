4dbe8c8ee8f0cd41a847bf4d7aa390d0b772e0ea
cassidypignatello <cassidy.pignatello@gmail.com>
2018-02-14 20:52:03 -0500

    styles user information fields and custom radio buttons

--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
 import './css/index.css';
 const App = {
-  init: function(){
+  init: function() {
     this.addListeners();
   },
-  addListeners: function(){
+  addListeners: function() {
     const helpButton = document.querySelector('.accordion');
-    const closeButton = document.querySelector('.close-button');
-    const closeButtonText = document.querySelector('.close-button p');
-    const closeButtonIcon = document.querySelector('.close-button i');
-    const newIcon = document.createElement('i');
     helpButton.addEventListener('click', function() {
       this.classList.toggle('active');
       const panel = this.nextElementSibling;
@@ -19,9 +15,7 @@ const App = {
         this.style.borderBottomLeftRadius = '4px';
         this.style.borderBottomRightRadius = '4px';
         panel.style.maxHeight = null;
-        closeButtonText.innerHTML = 'open';
-        closeButtonIcon.classList.remove('fa-long-arrow-up');
-        closeButtonIcon.classList.add('fa-long-arrow-down');
+        handleCloseButton('open', 'fa-long-arrow-up', 'fa-long-arrow-down');
       } else {
         panel.style.display = 'block';
         panel.style.borderTop = 'none';
@@ -31,10 +25,30 @@ const App = {
         panel.style.borderTopLeftRadius = 0;
         panel.style.borderTopRightRadius = 0;
         panel.style.maxHeight = panel.scrollHeight + "px";
-        closeButtonText.innerHTML = 'close';
-        closeButtonIcon.classList.remove('fa-long-arrow-down');
-        closeButtonIcon.classList.add('fa-long-arrow-up');
+        handleCloseButton('close', 'fa-long-arrow-down', 'fa-long-arrow-up');
       }
+    });
+    const handleCloseButton = (text, addClass, removeClass) => {
+      const closeButtonText = document.querySelector('.close-button p');
+      const closeButtonIcon = document.querySelector('.close-button i');
+      closeButtonText.innerHTML = text;
+      closeButtonIcon.classList.remove(removeClass);
+      closeButtonIcon.classList.add(addClass);
+    }
+
+    const inputs = document.querySelectorAll('.user-info-left input');
+    inputs.forEach((input) => {
+      input.addEventListener('focus', function() {
+        this.nextElementSibling.style.display = 'block';
+        // this.nextElementSibling.style.marginBottom = '5px';
+        this.style.marginBottom = 0;
+      }, true);
+
+      input.addEventListener('blur', function() {
+        this.nextElementSibling.style.display = 'none';
+        // this.nextElementSibling.style.marginBottom = 0;
+        this.style.marginBottom = '30px';
+      }, true);
     });
   }
 }
